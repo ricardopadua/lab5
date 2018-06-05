7f9e97ca9360da3344474652e0406b4c750f7f2d
Ricardo Pádua <ricardo.padua4@gmail.com>
2018-06-05 14:20:13 -0300

    Update index.js

--- lab5-service/index.js
+++ lab5-service/index.js
@@ -1,8 +1,7 @@
 // index.js
 const main = require("./src/main")
-<<<<<<< HEAD
+
 console.log("Server Entry Point! -  is running here http://localhost:3000")
-=======
+
 console.log("Server Entry Point!")
->>>>>>> 8bee194d69972d1adf8a518a89749648d653fe72
 main.start(3000)
