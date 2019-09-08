389535be74ec7a9460146b6afc17e56a45b16379
Pádua <ricardo.padua4@gmail.com>
2019-09-08 18:10:45 -0300

    delete folders

--- lab5-service/index.js
+++
@@ -1,5 +0,0 @@
-// index.js
-const main = require("./src/main")
-
-console.log("Server Entry Point! -  is running here http://localhost:3000")
-main.start(3000)
--- lab5-service/src/db.js
+++
@@ -1,4 +0,0 @@
-// src/db.js
-const cfg = require("../knexfile")
-const knex = require("knex")(cfg.development)
-exports.knex = knex
